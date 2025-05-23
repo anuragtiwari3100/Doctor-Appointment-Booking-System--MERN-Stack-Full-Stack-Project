6f6ccf99b04b11e0cdcc7c36f7000eca4917e039
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2025-05-23 11:18:28 +0530

    Define app routes in App.jsx and complete Banner, Header, and Footer components

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+          <li>📞 +91-6204837397</li>
+       <li>📧100anuragtiwari.dev@gmail.com</li>
+            <li>📍 1234 Health St, Wellness City</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>© 2024 Tiwari.com — Made with ❤️ for a healthier tomorrow.
+        </p>
+      </div>
+
+    </div>
   )
 }
 
