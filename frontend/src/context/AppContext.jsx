4b1b9b0bf8837a20f4dc08d57641fd9e9356d4d3
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2025-05-25 23:20:06 +0530

    Refactored doctor components, updated AppContext logic, and modified .gitignore

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import axios from 'axios'
 
-const AppContext = () => {
-  return (
-    <div>AppContext</div>
-  )
+export const AppContext = createContext()
+
+const AppContextProvider = (props) => {
+
+    const currencySymbol = '₹'
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const [doctors, setDoctors] = useState([])
+    const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
+    const [userData, setUserData] = useState(false)
+    useEffect(() => {
+        getDoctosData()
+    }, [])
+
+    useEffect(() => {
+        if (token) {
+            loadUserProfileData()
+        }
+    }, [token])
+
+    const value = {
+        doctors, getDoctosData,
+        currencySymbol,
+        backendUrl,
+        token, setToken,
+        userData, setUserData, loadUserProfileData
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+
 }
 
-export default AppContext
+export default AppContextProvider
